ab57f3d877389fa44d6910e8c601cb546be652da
Aelyce11 <alicebo.1717@gmail.com>
2020-06-21 19:01:47 +0200

    added regexp verif

--- hashid.py
+++ hashid.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import re
 
 
 def parse_args() -> argparse.Namespace:
@@ -15,20 +16,44 @@ def parse_args() -> argparse.Namespace:
 
 
 def analyze(hash):
-    result = []
-    for i in algorithm_list.items():
+    result_all = []
+    result_most = []
+    result_least = []
+
+    for i in algorithm_list_len.items():
         if len(hash) == i[1]:
-            result.append(i[0])
+            result_all.append(i[0])
 
-    if len(result) != 0:
-        return result
-    return ["Unknown hash algorithm"]
+    if len(result_all) == 0:
+        return ["No correspondance found."]
 
-algorithm_list = {
-    # "Sdbm": ?,
-    # "Loselose": ?,
-    # "Pearson": ?,
-    # "Base64": ?,
+    for i in result_all:
+        if i in algorithm_list_charset.keys():
+            if (algorithm_list_charset.get(i)).full match(hash):
+                result_most.append(i)
+        else:
+            result_least.append(i)
+
+    m = ["MOST PROBABLE ALGORITHM :"] + result_most
+    l = ["LESS PROBABLE ALGORITHM :"] + result_least
+
+    n = m + [""] + l
+
+    return n
+
+
+algorithm_list_charset = {
+    "MD5": re.compile(r"[a-fA-F0-9]*"),
+    "Sha-1": re.compile(r"[a-fA-F0-9]*"),
+    "Sha-224": re.compile(r"[a-fA-F0-9]*"),
+    "Sha-256": re.compile(r"[a-fA-F0-9]*"),
+    "Sha-384": re.compile(r"[a-fA-F0-9]*"),
+    "Sha-512": re.compile(r"[a-fA-F0-9]*"),
+    "Blowfish": re.compile(r"$2a\$\08\$(.)*"),
+    # "": re.compile(r""),
+}
+
+algorithm_list_len = {
     "Djb-2": 5,
     "Adler-32": 8,
     "Crc-32": 8,
@@ -78,6 +103,7 @@ algorithm_list = {
     "Haval-256_3": 64,
     "Haval-256_4": 64,
     "Haval-256_5": 64,
+    "Blowfish": 80,
     "Ripemd-320": 80,
     "Sha-384": 96,
     "Sha-512": 128,
@@ -85,6 +111,8 @@ algorithm_list = {
     "Fnv-1a512": 128,
     "Fnv-1a1024": 256,
 }
+
+# BLAKE
 
 def main() -> None:
     args = parse_args()
